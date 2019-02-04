7d993558bd6e4f219aaf776d496eac6bd88b2d6f
Brian Richter <brianorwhatever@gmail.com>
2019-02-03 23:51:09 -0800

    remove bad import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SigComponent } from './sig/sig.component';
 
 @NgModule({
    declarations: [
-      AppComponent,
-      SigComponent
+      AppComponent
    ],
    imports: [
       BrowserModule
